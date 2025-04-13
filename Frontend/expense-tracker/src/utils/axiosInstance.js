52c6d96afad9b80fed06d847ce802edc811846a7
PrasunPrabhat <prasun2000kir@gmail.com>
2025-04-13 18:50:21 +0530

    API FLOW CHART PDF

--- Frontend/expense-tracker/src/utils/axiosInstance.js
+++ Frontend/expense-tracker/src/utils/axiosInstance.js
@@ -13,27 +13,38 @@ const axiosInstance = axios.create({
 
 // Request Intercepter
 axiosInstance.interceptors.request.use(
-    (config) => {
-        const accessToken = localStorage.getItem("token");
-        if(accessToken) {
-            config.headers.Authorization = `Bearer ${accessToken}`;
-        }
+  (config) => {
+    const accessToken = localStorage.getItem("token");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
 
-        return config
-    },
-    (error) => {
-        return Promise.reject(error);
-    },
-)
-
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 // Response Intercepter
 axiosInstance.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    (error) => {
-        // Handle common errors globally
-        if(error.res)
-    },
-)
+  (response) => {
+    return response;
+  },
+  (error) => {
+    // Handle common errors globally
+    if (error.response) {
+      if (error.response.status === 401) {
+        // Redirect to Login page
+        window.location.href = "/login";
+      } else if (error.response.status === 500) {
+        console.error("Server error. Please try again later.");
+      }
+    } else if (error.code === "ECONNABORTED") {
+      console.error("Request timeout. Please try again.");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
